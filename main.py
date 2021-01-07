7ff7bd7eaa6068934db5d0ebcfce091bb0f6d3bd
Charles Lambrecht <lambrechtca@gmail.com>
2021-01-07 20:47:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import discord
+import os
+import datetime
+from collections.abc import Sequence
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    cmd = message.content
+
+    if cmd.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if cmd.startswith('$wheel'):
+      #get system time
+      #commandTime = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+      #print(commandTime)
+
+      #get users
+      author = message.author
+      y = author
+      userProfiles = [y]
+      for member in message.mentions:
+        userProfiles.append(member)
+
+      print(userProfiles)
+
+      for x in userProfiles:
+        await x.send("Please respond with your chosen number")
+        response = await client.wait_for('message', timeout=120.0,check=message_check(channel=x.dm_channel))
+        print(response.content)
+        #await message.channel.send(x.mention)
+
+def make_sequence(seq):
+    if seq is None:
+        return ()
+    if isinstance(seq, Sequence) and not isinstance(seq, str):
+        return seq
+    else:
+        return (seq,)
+
+def message_check(channel=None, author=None, content=None, ignore_bot=True, lower=True):
+  channel = make_sequence(channel)
+  author = make_sequence(author)
+  content = make_sequence(content)
+  if lower:
+      content = tuple(c.lower() for c in content)
+  def check(message):
+      if ignore_bot and message.author.bot:
+          return False
+      if channel and message.channel not in channel:
+          return False
+      if author and message.author not in author:
+          return False
+      actual_content = message.content.lower() if lower else message.content
+      if content and actual_content not in content:
+          return False
+      return True
+  return check
+
+client.run(os.getenv('TOKEN'))
